fa42531e03172b62323f70821d60d95d30df5254
xSalice <xSalicez@gmail.com>
2014-11-25 00:54:29 -0600

    update

---
+++ xSaliceReligionAIO/xSaliceReligionAIO/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LeagueSharp.Common;
+
+namespace xSaliceReligionAIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomEvents.Game.OnGameLoad += LoadReligion;
+        }
+
+        static void LoadReligion(EventArgs args)
+        {
+            Champion champs = new Champion(true);
+        }
+    }
+}
