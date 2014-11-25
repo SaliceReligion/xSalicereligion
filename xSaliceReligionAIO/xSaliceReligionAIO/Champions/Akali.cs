2cffe7f48c34929f50f3d48446765b109fe393fe
xSalice <xSalicez@gmail.com>
2014-11-25 16:25:30 -0600

    rawr

--- xSaliceReligionAIO/xSaliceReligionAIO/Champions/Akali.cs
+++ xSaliceReligionAIO/xSaliceReligionAIO/Champions/Akali.cs
@@ -390,8 +390,8 @@ namespace xSaliceReligionAIO.Champions
             {
                 if (R.IsKillable(target) && menu.Item("R_If_Killable").GetValue<bool>())
                     R.Cast(target, packets());
-                else if (getSimpleDmg(target) > target.Health)
-                    R.Cast(target, packets());
+                //else if (getSimpleDmg(target) > target.Health)
+                   // R.Cast(target, packets());
                 if (mode == 0)
                 {
                     if (menu.Item("R_Wait_For_Q").GetValue<bool>())
